97cadffdc665909c252da0c0de2f16b2d1f4e513
Luke Satchwell <satch744@googlemail.com>
2023-06-07 12:08:36 +0100

    shopping cart next js and redux

---
+++ app/category/[category]/page.jsx
@@ -0,0 +1,27 @@
+"use client";
+import { useRouter } from "next/navigation";
+import ProductCard from "/components/ProductCard";
+import styles from "/styles/ShopPage.module.css";
+import { getProductsByCategory } from "@/app/api/products/[category]";
+
+const CategoryPage = ({ products }) => {
+  const router = useRouter();
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Results for {router.query.category}</h1>
+      <div className={styles.cards}>
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPage;
+
+export async function getServerSideProps(ctx) {
+  const category = ctx.query.category;
+  const products = await getProductsByCategory(category);
+  return { props: { products } };
+}
